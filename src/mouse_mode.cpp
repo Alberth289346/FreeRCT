34e1633d79d6ee9bd7115c39d85b155ef2cc5f18
Alberth <alberth289346@gmail.com>
2015-03-21 18:07:37 +0100

    Add: Mouse mode with cursor area.

--- src/mouse_mode.cpp
+++ src/mouse_mode.cpp
@@ -9,6 +9,9 @@
 
 #include "stdafx.h"
 #include "mouse_mode.h"
+#include "math_func.h"
+#include "map.h"
+#include "viewport.h"
 
 MouseModeSelector::MouseModeSelector()
 {
@@ -18,7 +21,110 @@ MouseModeSelector::~MouseModeSelector()
 {
 }
 
-/** The voxels changed by the selector should be redrawn. */
+/** Mark all voxels changed by the selector as dirty, so they get redrawn. */
 void MouseModeSelector::MarkDirty()
 {
 }
+
+/**
+ * \fn CursorMouseMode MouseModeSelector::GetCursor(const XYZPoint16 &voxel_pos) const
+ * Retrieve the cursor to display at the given voxel.
+ * @param voxel_pos %Voxel to decorate with a cursor.
+ * @return The cursor to use, or #CUR_TYPE_INVALID if no cursor should be displayed.
+ */
+
+CursorMouseMode::CursorMouseMode() : MouseModeSelector()
+{
+}
+
+CursorMouseMode::~CursorMouseMode()
+{
+}
+
+/**
+ * Set the size of the cursor area.
+ * @param xsize Horizontal size of the area.
+ * @param ysize Vertical size of the area.
+ */
+void CursorMouseMode::SetSize(int xsize, int ysize)
+{
+	xsize = Clamp(xsize, 0, 128); // Arbitrary upper limit.
+	ysize = Clamp(ysize, 0, 128);
+	this->area.width = xsize;
+	this->area.height = ysize;
+	this->InitTileData();
+}
+
+/** Initialize the tile data of the cursor area. */
+void CursorMouseMode::InitTileData()
+{
+	if (this->area.width == 0 || this->area.height == 0) return;
+
+	/* Setup the cursor area for the current position and size. */
+	int size = this->area.width * this->area.height;
+	this->ground_height.resize(size);
+	for (int x = 0; x < this->area.width; x++) {
+		int xpos = this->area.base.x + x;
+		for (int y = 0; y < this->area.height; y++) {
+			int ypos = this->area.base.y + y;
+			TileData &td = this->GetTileData(x, y);
+			td.enabled = xpos >= 0 && xpos < _world.GetXSize() && ypos >= 0 && ypos < _world.GetYSize() &&
+					_world.GetTileOwner(xpos, ypos) == OWN_PARK;
+			td.height = -1;
+		}
+	}
+}
+
+/**
+ * Set the position of the cursor area.
+ * @param xbase X coordinate of the base position.
+ * @param ybase Y coordinate of the base position.
+ */
+void CursorMouseMode::SetPosition(int xbase, int ybase)
+{
+	this->area.base.x = xbase;
+	this->area.base.y = ybase;
+	this->InitTileData();
+}
+
+/**
+ * Get the top height of ground at the given world voxel stack.
+ * @param [inout] td Til data height cache.
+ * @param xpos X position of the queried voxel stack.
+ * @param ypos Y position of the queried voxel stack.
+ * @return Top voxel height containing ground.
+ */
+static inline int GetTopGroundHeight(TileData &td, int xpos, int ypos)
+{
+	if (td.height >= 0) return td.height;
+	td.height = _world.GetTopGroundHeight(xpos, ypos);
+	return td.height;
+}
+
+CursorType CursorMouseMode::GetCursor(const XYZPoint16 &voxel_pos)
+{
+	int x = voxel_pos.x - this->area.base.x;
+	if (x < 0 || x >= this->area.width) return CUR_TYPE_INVALID;
+
+	int y = voxel_pos.y - this->area.base.y;
+	if (y < 0 || y >= this->area.height) return CUR_TYPE_INVALID;
+
+	TileData &td = this->GetTileData(x, y);
+	if (td.enabled && GetTopGroundHeight(td, voxel_pos.x, voxel_pos.y) == voxel_pos.z) return CUR_TYPE_TILE;
+	return CUR_TYPE_INVALID;
+}
+
+void CursorMouseMode::MarkDirty()
+{
+	for (int x = 0; x < this->area.width; x++) {
+		int xpos = this->area.base.x + x;
+		for (int y = 0; y < this->area.height; y++) {
+			int ypos = this->area.base.y + y;
+			TileData &td = this->GetTileData(x, y);
+			if (!td.enabled) continue;
+
+			MarkVoxelDirty(XYZPoint16(xpos, ypos, GetTopGroundHeight(td, xpos, ypos)), 0);
+		}
+	}
+}
+
--- src/mouse_mode.h
+++ src/mouse_mode.h
@@ -10,6 +10,30 @@
 #ifndef MOUSE_MODE_H
 #define MOUSE_MODE_H
 
+#include "geometry.h"
+
+/**
+ * Available cursor types.
+ * @ingroup viewport_group
+ */
+enum CursorType {
+	CUR_TYPE_NORTH,    ///< Show a N corner highlight.
+	CUR_TYPE_EAST,     ///< Show a E corner highlight.
+	CUR_TYPE_SOUTH,    ///< Show a S corner highlight.
+	CUR_TYPE_WEST,     ///< Show a W corner highlight.
+	CUR_TYPE_TILE,     ///< Show a tile highlight.
+	CUR_TYPE_ARROW_NE, ///< Show a build arrow in the NE direction.
+	CUR_TYPE_ARROW_SE, ///< Show a build arrow in the SE direction.
+	CUR_TYPE_ARROW_SW, ///< Show a build arrow in the SW direction.
+	CUR_TYPE_ARROW_NW, ///< Show a build arrow in the NW direction.
+	CUR_TYPE_EDGE_NE,  ///< Show a NE edge sprite highlight.
+	CUR_TYPE_EDGE_SE,  ///< Show a SE edge sprite highlight.
+	CUR_TYPE_EDGE_SW,  ///< Show a SW edge sprite highlight.
+	CUR_TYPE_EDGE_NW,  ///< Show a NW edge sprite highlight.
+
+	CUR_TYPE_INVALID = 0xFF, ///< Invalid/unused cursor.
+};
+
 /** Base class for displaying and handling mouse modes. */
 class MouseModeSelector {
 public:
@@ -17,6 +41,52 @@ public:
 	virtual ~MouseModeSelector();
 
 	virtual void MarkDirty();
+	virtual CursorType GetCursor(const XYZPoint16 &voxel_pos) = 0;
+
+	/**
+	 * Rough estimate whether the selector wants to render something in the voxel stack at the given coordinate.
+	 * @param x X position of the stack.
+	 * @param y Y position of the stack.
+	 * @return Whether the selector wants to contribute to the graphics in the given stack.
+	 */
+	inline bool IsInsideArea(int x, int y) const {
+		return this->area.IsPointInside(x, y);
+	}
+
+	Rectangle16 area; ///< Position and size of the selected area (over-approximation of voxel stacks).
+};
+
+/** Data of a tile. */
+struct TileData {
+	int8 height;  ///< Height of the cursor (equal to ground height, except at steep slopes). Negative value means 'unknown'.
+	bool enabled; ///< Whether the tile should have a cursor displayed.
+};
+
+/** Mouse mode displaying a tile cursor of some size at the ground. */
+class CursorMouseMode : public MouseModeSelector {
+public:
+	CursorMouseMode();
+	~CursorMouseMode();
+
+	virtual void MarkDirty() override;
+	virtual CursorType GetCursor(const XYZPoint16 &voxel_pos) override;
+
+	void SetSize(int xsize, int ysize);
+	void SetPosition(int xbase, int ybase);
+	void InitTileData();
+
+	/**
+	 * Get the tile data at the given relative position.
+	 * @param dx X position relative to the base position.
+	 * @param dy Y position relative to the base position.
+	 * @return Reference to the tile data.
+	 */
+	inline TileData &GetTileData(int dx, int dy)
+	{
+		return this->ground_height[dx * this->area.height + dy];
+	}
+
+	std::vector<TileData> ground_height; ///< Height of the ground within the area, negative means 'unknown'.
 };
 
 #endif
