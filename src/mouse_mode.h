d00451d414a8516d14544fe5b87e3a8dfb817b46
Alberth <alberth289346@gmail.com>
2015-04-04 14:30:30 +0200

    Change: Rewrite mouse mode implementation.

--- src/mouse_mode.h
+++ src/mouse_mode.h
@@ -11,6 +11,8 @@
 #define MOUSE_MODE_H
 
 #include "geometry.h"
+#include "math_func.h"
+#include "map.h"
 
 /**
  * Available cursor types.
@@ -34,6 +36,8 @@ enum CursorType {
 	CUR_TYPE_INVALID = 0xFF, ///< Invalid/unused cursor.
 };
 
+static const uint32 INVALID_TILE_INDEX = 0xFFFFu; ///< Invalid selector tile index.
+
 /** Base class for displaying and handling mouse modes. */
 class MouseModeSelector {
 public:
@@ -45,31 +49,228 @@ public:
 	virtual uint32 GetRange(uint xpos, uint ypos) = 0;
 
 	/**
+	 * Get the ride data of a voxel if available, for rendering.
+	 * @param voxel %Voxel being rendered.
+	 * @param voxel_pos Position of the voxel in the world.
+	 * @param [out] sri Ride instance that should be rendered if \c true is returned.
+	 * @param [out] instance_data Instance data that should be rendered if \c true is returned.
+	 * @return \c True if the data supplied through \a sri and \a instance_data should be used, else \c false.
+	 */
+	virtual bool GetRide(const Voxel *voxel, const XYZPoint16 &voxel_pos, SmallRideInstance *sri, uint16 *instance_data)
+	{
+		return false;
+	}
+
+	/**
+	 * Get the offset of the tile position in the area. Parameters are unchecked.
+	 * @param x Horizontal relative offset in the area.
+	 * @param y Vertical relative offset in the area.
+	 * @return Index in the tile data.
+	 */
+	inline uint32 GetTileOffset(int rel_x, int rel_y) const
+	{
+		return rel_x * this->area.height + rel_y;
+	}
+
+	/**
+	 * Get the index of the tile position in the area.
+	 * @param x Horizontal coordinate of the absolute world position.
+	 * @param y Vertical coordinate of the absolute world position.
+	 * @return Index in the tile data, or #INVALID_TILE_INDEX if out of bounds.
+	 */
+	inline uint32 GetTileIndex(int x, int y) const {
+		x -= this->area.base.x;
+		if (x < 0 || x >= this->area.width) return INVALID_TILE_INDEX;
+		y -= this->area.base.y;
+		if (y < 0 || y >= this->area.height) return INVALID_TILE_INDEX;
+		return GetTileOffset(x, y);
+	}
+
+	/**
+	 * Get the index of the tile position in the area.
+	 * @param pos Absolute world position.
+	 * @return Index in the tile data, or #INVALID_TILE_INDEX if out of bounds.
+	 */
+	inline uint32 GetTileIndex(const Point16 &pos) const {
+		return this->GetTileIndex(pos.x, pos.y);
+	}
+
+	/**
 	 * Rough estimate whether the selector wants to render something in the voxel stack at the given coordinate.
 	 * @param x X position of the stack.
 	 * @param y Y position of the stack.
 	 * @return Whether the selector wants to contribute to the graphics in the given stack.
 	 */
 	inline bool IsInsideArea(int x, int y) const {
-		return this->area.IsPointInside(x, y);
+		return this->GetTileIndex(x, y) != INVALID_TILE_INDEX;
 	}
 
 	Rectangle16 area; ///< Position and size of the selected area (over-approximation of voxel stacks).
 };
 
-/** Data of a tile. */
-struct TileData {
+/** Cursor data of a tile. */
+struct CursorTileData {
 	int8 cursor_height;  ///< Height of the cursor (equal to ground height, except at steep slopes). Negative value means 'unknown'.
-	uint8 lowest;        ///< Lowest voxel that should be rendered.
-	uint8 highest;       ///< Highest voxel that should be rendered.
 	bool cursor_enabled; ///< Whether the tile should have a cursor displayed.
+
+	/** Initialize #CursorTileData data members. */
+	virtual void Init()
+	{
+		this->cursor_height = -1;
+		this->cursor_enabled = false;
+	}
+
+	/**
+	 * Get the height of the ground at the tile (top-voxel in case of steep slope).
+	 * @param abs_x Absolute world X position of the tile.
+	 * @param abs_y Absolute world Y position of the tile.
+	 * @return Height of the ground at the tile (top-voxel in case of steep slope).
+	 */
+	int8 GetGroundHeight(int abs_x, int abs_y)
+	{
+		if (this->cursor_height >= 0) return this->cursor_height;
+		this->cursor_height = _world.GetTopGroundHeight(abs_x, abs_y);
+		return this->cursor_height;
+	}
+};
+
+void MarkVoxelDirty(const XYZPoint16 &voxel_pos, int16 height); // viewport.cpp
+
+/** Mouse mode displaying a tile cursor of some size at the ground. */
+template <typename TileData>
+class TileDataMouseMode : public MouseModeSelector {
+public:
+	TileDataMouseMode() : MouseModeSelector()
+	{
+		this->cur_cursor = CUR_TYPE_TILE; // Use a visible cursor by default.
+	}
+
+	~TileDataMouseMode()
+	{
+	}
+
+	void MarkDirty() override
+	{
+		for (int x = 0; x < this->area.width; x++) {
+			int xpos = this->area.base.x + x;
+			for (int y = 0; y < this->area.height; y++) {
+				int ypos = this->area.base.y + y;
+				TileData &td = this->tile_data[this->GetTileOffset(x, y)];
+				if (!td.cursor_enabled || td.cursor_height < 0) continue;
+
+				MarkVoxelDirty(XYZPoint16(xpos, ypos, td.cursor_height), 0);
+			}
+		}
+	}
+
+	CursorType GetCursor(const XYZPoint16 &voxel_pos) override
+	{
+		uint32 index = this->GetTileIndex(voxel_pos.x, voxel_pos.y);
+		if (index == INVALID_TILE_INDEX) return CUR_TYPE_INVALID;
+		TileData &td = this->tile_data[index];
+		if (td.cursor_enabled && td.GetGroundHeight(voxel_pos.x, voxel_pos.y) == voxel_pos.z) return this->cur_cursor;
+		return CUR_TYPE_INVALID;
+	}
+
+	uint32 GetRange(uint xpos, uint ypos) override
+	{
+		return 0;
+	}
+
+	/**
+	 * Set the size of the cursor area.
+	 * @param xsize Horizontal size of the area.
+	 * @param ysize Vertical size of the area.
+	 * @pre World must have been marked dirty before moving the area.
+	 * @post World must be marked dirty after moving the area.
+	 */
+	void SetSize(int xsize, int ysize)
+	{
+		xsize = Clamp(xsize, 0, 128); // Arbitrary upper limit.
+		ysize = Clamp(ysize, 0, 128);
+		this->area.width = xsize;
+		this->area.height = ysize;
+		this->InitTileData();
+	}
+
+	/**
+	 * Set the position of the cursor area. Clears the cursor and range data.
+	 * @param xbase X coordinate of the base position.
+	 * @param ybase Y coordinate of the base position.
+	 * @pre World must have been marked dirty before moving the area.
+	 * @post World must be marked dirty after moving the area.
+	 */
+	void SetPosition(int xbase, int ybase)
+	{
+		this->area.base.x = xbase;
+		this->area.base.y = ybase;
+		this->InitTileData();
+	}
+
+	/** Initialize the tile data of the cursor area. */
+	void InitTileData()
+	{
+		if (this->area.width == 0 || this->area.height == 0) return;
+
+		/* Setup the cursor area for the current position and size. */
+		int size = this->area.width * this->area.height;
+		this->tile_data.resize(size);
+		for (int x = 0; x < this->area.width; x++) {
+			int xpos = this->area.base.x + x;
+			for (int y = 0; y < this->area.height; y++) {
+				int ypos = this->area.base.y + y;
+				TileData &td = this->tile_data[this->GetTileOffset(x, y)];
+				td.Init();
+				td.cursor_enabled = xpos >= 0 && xpos < _world.GetXSize() && ypos >= 0 && ypos < _world.GetYSize() &&
+						_world.GetTileOwner(xpos, ypos) == OWN_PARK;
+			}
+		}
+	}
+
+	std::vector<TileData> tile_data; ///< Tile data of the area.
+	CursorType cur_cursor;           ///< Cursor to return at the #GetCursor call.
+};
+
+typedef TileDataMouseMode<CursorTileData> CursorMouseMode;
+
+/** Ride information of a voxel. */
+struct VoxelRideData {
+	SmallRideInstance sri;
+	uint16 instance_data;
+};
+
+
+/** %Tile data with ride voxel information. */
+struct RideTileData : public CursorTileData {
+	uint8 lowest;  ///< Lowest voxel in the stack that should be rendered.
+	uint8 highest; ///< Highest voxel in the stack that should be rendered.
+	std::vector<VoxelRideData> ride_info; ///< Ride information of voxels RideTileData::lowest to RideTileData::highest (inclusive).
+
+	/** Initialize #RideTileData and #CursorTileData data members. */
+	void Init() override
+	{
+		this->CursorTileData::Init();
+		this->lowest = 1;
+		this->highest = 0;
+	}
+
+	/**
+	 * After initializing the  voxels RideTileData::lowest and RideTileData::highest data members, initialize
+	 * the ride data for all voxels in-between.
+	 */
+	void SetupRideInfoSpace()
+	{
+		int size = std::max(0, this->highest - this->lowest + 1);
+		this->ride_info.resize(size);
+		for (int z = 0; z < size; z++) this->ride_info[z].sri = SRI_FREE;
+	}
 
 	/**
 	 * Add a (z-position) of a voxel to the vertical voxel range to render.
 	 * @param zpos Vertical voxel position to add.
-	 * @see CursorMouseMode::GetRange
+	 * @see TileDataMouseMode::GetRange
 	 */
-	inline void AddVoxel(uint8 zpos)
+	void AddVoxel(uint8 zpos)
 	{
 		if (this->lowest > this->highest) {
 			this->lowest = zpos;
@@ -79,35 +280,106 @@ struct TileData {
 			this->highest = std::max(this->highest, zpos);
 		}
 	}
-};
-
-/** Mouse mode displaying a tile cursor of some size at the ground. */
-class CursorMouseMode : public MouseModeSelector {
-public:
-	CursorMouseMode();
-	~CursorMouseMode();
-
-	virtual void MarkDirty() override;
-	virtual CursorType GetCursor(const XYZPoint16 &voxel_pos) override;
-	virtual uint32 GetRange(uint xpos, uint ypos) override;
-
-	void SetSize(int xsize, int ysize);
-	void SetPosition(int xbase, int ybase);
-	void InitTileData();
 
 	/**
-	 * Get the tile data at the given relative position.
-	 * @param dx X position relative to the base position.
-	 * @param dy Y position relative to the base position.
-	 * @return Reference to the tile data.
+	 * Set the ride data of a voxel.
+	 * @param zpos Z position of the voxel.
+	 * @param sri Ride instance number.
+	 * @param instance_data Instance data.
+	 * @pre All voxels must have been added before (RideTileData::AddVoxel), and
+	 *      space must have been set up (RideTileData::SetupRideInfoSpace).
 	 */
-	inline TileData &GetTileData(int dx, int dy)
+	void SetRideData(uint8 zpos, SmallRideInstance sri, uint16 instance_data)
 	{
-		return this->ground_height[dx * this->area.height + dy];
+		if (zpos >= this->lowest && zpos <= this->highest) {
+			VoxelRideData &vrd = this->ride_info[zpos - this->lowest];
+			vrd.sri = sri;
+			vrd.instance_data = instance_data;
+		}
+	}
+};
+
+template <typename TileData>
+class RideTileDataMouseMode : public TileDataMouseMode<TileData> {
+public:
+	RideTileDataMouseMode() : TileDataMouseMode<TileData>()
+	{
 	}
 
-	std::vector<TileData> ground_height; ///< Height of the ground within the area, negative means 'unknown'.
-	CursorType cur_cursor;               ///< Cursor to return at the #GetCursor call.
+	~RideTileDataMouseMode()
+	{
+	}
+
+	bool GetRide(const Voxel *voxel, const XYZPoint16 &voxel_pos, SmallRideInstance *sri, uint16 *instance_data) override
+	{
+		uint32 index = this->GetTileIndex(voxel_pos.x, voxel_pos.y);
+		if (index == INVALID_TILE_INDEX) return false;
+
+		const TileData &td = this->tile_data[index];
+		if (!td.cursor_enabled || voxel_pos.z < td.lowest || voxel_pos.z > td.highest) return false;
+
+		const VoxelRideData &vrd = td.ride_info[voxel_pos.z - td.lowest];
+		*sri = vrd.sri;
+		*instance_data = vrd.instance_data;
+		printf("getting vrd: sri=%d, instdata=%d, z=%d\n", *sri, *instance_data, voxel_pos.z);
+		return (vrd.sri != SRI_FREE);
+	}
+
+	/**
+	 * Denote that the given voxel will contain part of a ride.
+	 * @param pos Absolute world position.
+	 */
+	void AddVoxel(const XYZPoint16 &pos)
+	{
+		uint32 index = this->GetTileIndex(pos.x, pos.y);
+		assert(index != INVALID_TILE_INDEX);
+		TileData &td = this->tile_data[index];
+		td.cursor_enabled = true;
+		td.AddVoxel(pos.z);
+	}
+
+	/**
+	 * Setup space for the ride information.
+	 * @pre RideTileDataMouseMode::AddVoxel must have been done.
+	 */
+	void SetupRideInfoSpace()
+	{
+		for (int x = 0; x < this->area.width; x++) {
+			for (int y = 0; y < this->area.height; y++) this->tile_data[this->GetTileOffset(x, y)].SetupRideInfoSpace();
+		}
+	}
+
+	/**
+	 * Set ride data at the given position in the area. Disabled tiles are silently skipped.
+	 * @param pos World position.
+	 * @param sri Ride instance number.
+	 * @param instance_data Instance data.
+	 */
+	void SetRideData(const XYZPoint16 &pos, SmallRideInstance sri, uint16 instance_data)
+	{
+		uint32 index = this->GetTileIndex(pos.x, pos.y);
+		assert(index != INVALID_TILE_INDEX);
+		TileData &td = this->tile_data[index];
+		if (!td.cursor_enabled) return;
+		VoxelRideData &vrd = td.ride_info[pos.z - td.lowest];
+		vrd.sri = sri;
+		vrd.instance_data = instance_data;
+	}
+
+	void MarkDirty() override
+	{
+		for (int x = 0; x < this->area.width; x++) {
+			int xpos = this->area.base.x + x;
+			for (int y = 0; y < this->area.height; y++) {
+				int ypos = this->area.base.y + y;
+				TileData &td = this->tile_data[this->GetTileOffset(x, y)];
+				if (!td.cursor_enabled || td.cursor_height < 0) continue;
+
+				MarkVoxelDirty(XYZPoint16(xpos, ypos, td.cursor_height), 0);
+				if (td.lowest <= td.highest) MarkVoxelDirty(XYZPoint16(xpos, ypos, td.lowest), td.highest - td.lowest + 1);
+			}
+		}
+	}
 };
 
 #endif
